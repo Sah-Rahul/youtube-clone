2839763de4b38b8b7d2a4dd5a18df9ba9e9b4287
RahulSah <sahfamilyinfo@gmail.com>
2025-09-15 20:23:33 +0545

    utils setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import ConnectDb from "./src/db/db.js";
+import app from "./src/app.js";
 dotenv.config()
 
 
@@ -7,6 +8,13 @@ dotenv.config()
 
 
 ConnectDb()
-
+.then(() =>{
+    app.listen(process.env.PORT || 400, () =>{
+        console.log(`server is runnning at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log(`MONGO DB CONNECTION FAILED`, err)
+})
 
  
---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// CORS setup
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+export default app;
