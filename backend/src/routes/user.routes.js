1147361cfd9392733e84bd45b9beb5c92f0e74cb
RahulSah <sahfamilyinfo@gmail.com>
2025-09-19 14:45:50 +0545

    added getWatchHistory and getUserChannelProfile api

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -8,6 +8,9 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   changeCurrentPassword,
+  getUserChannelProfile,
+  getWatchHistory,
+  // updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -28,7 +31,11 @@ userRouter.post("/logout", verifyJWT, logoutUser);
 userRouter.get("/me", verifyJWT, getMyProfile);
 userRouter.post("/refresh-token", refreshAccessToken);
 userRouter.put("/change-password", verifyJWT, changeCurrentPassword);
-userRouter.put("/update-account", verifyJWT, updateAccountDetails);
+userRouter.patch("/update-account", verifyJWT, updateAccountDetails);
 userRouter.put("/avatar", upload.single("avatar"), verifyJWT, updateUserAvatar);
+// userRouter.put("/cover-image", verifyJWT, upload.single("coverImage"),updateUserCoverImage
+// );
+userRouter.get("/getUser-Channel-Profile/:username",   verifyJWT, getUserChannelProfile);
+userRouter.get("/get-Watch-History",  verifyJWT, getWatchHistory);
 
 export default userRouter;
