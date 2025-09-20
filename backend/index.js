c1125fa40a34368030f123838cb26a7f33b552b7
RahulSah <sahfamilyinfo@gmail.com>
2025-09-20 23:06:57 +0545

    added video controller

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import ConnectDb from "./src/db/db.js";
 import app from "./src/app.js";
-dotenv.config()
+dotenv.config();
 
-
-
- 
 ConnectDb()
-.then(() =>{
-    app.listen(process.env.PORT || 400, () =>{
-        console.log(`server is runnning at port: ${process.env.PORT}`)
-    })
-})
-.catch((err) =>{
-    console.log(`MONGO DB CONNECTION FAILED`, err)
-})
-
- 
+  .then(() => {
+    app.listen(process.env.PORT || 400, () => {
+      console.log(`server is runnning at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGO DB CONNECTION FAILED`, err);
+  });
